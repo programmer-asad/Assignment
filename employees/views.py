c9c45eefe6cbca1d5229005be9cce3db8624b287
Md. Asadul Islam <01913404212.asad@gmail.com>
2024-09-16 09:57:14 +0600

    Module_16_exam_Assignment

---
+++ employees/views.py
@@ -0,0 +1,48 @@
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Employee
+from .forms import EmployeeForm, UpdateEmployeeForm
+
+
+
+# List all employees
+def employee_list(request):
+    employees = Employee.objects.all()
+    return render(request, 'employees/employee_list.html', {'employees': employees})
+
+
+
+# Create a new employee
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm()
+    return render(request, 'employees/add_employee.html', {'form': form})
+
+
+
+# Update employee information
+def update_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    if request.method == 'POST':
+        form = UpdateEmployeeForm(request.POST, instance=employee)
+        if form.is_valid():
+            form.save()
+            return redirect('employee_list')
+    else:
+        form = UpdateEmployeeForm(instance=employee)
+    return render(request, 'employees/update_employee.html', {'form': form})
+
+
+
+# Delete an employee
+def delete_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    if request.method == 'POST':
+        employee.delete()
+        return redirect('employee_list')
+    return render(request, 'employees/delete_employee.html', {'employee': employee})
